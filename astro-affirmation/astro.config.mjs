628bcfa57a3edb847394032ea18c818690b6d0b3
Ádám Sarok <sarok.adam@gmail.com>
2025-07-04 23:47:34 +0200

    chore: cleanup

--- astro-affirmation/astro.config.mjs
+++ astro-affirmation/astro.config.mjs
@@ -1,19 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import sanity from '@sanity/astro';
 import react from '@astrojs/react';
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [ sanity({
-    projectId: 'xz16h32m',
-    dataset: 'production',
-    useCdn: false,
-    apiVersion: "2025-06-15",
-    studioBasePath: '/studio',
-    stega: {
-      studioUrl: "/studio",
-    },
-  }), react()]
-});
+export default defineConfig({});
