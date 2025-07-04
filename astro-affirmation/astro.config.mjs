edf660fe25a8b2ef9e13791457a680fa32d761b4
Ádám Sarok <sarok.adam@gmail.com>
2025-07-05 00:32:36 +0200

    feat: random post

--- astro-affirmation/astro.config.mjs
+++ astro-affirmation/astro.config.mjs
@@ -4,4 +4,6 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'static'
+});
